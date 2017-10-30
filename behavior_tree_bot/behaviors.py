6abf3db046d21c94d29fd3cd48d5630778deb7c5
Harland Duman <dumanjacob@gmail.com>
2017-10-29 21:34:35 -0700

    Improved close planet strategy

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -19,9 +19,6 @@ def attack_weakest_enemy_planet(state):
         return False
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
-        logging.info(strongest_planet.num_ships)
-        logging.info(weakest_planet.num_ships)
-        logging.info(state.distance(strongest_planet.ID, weakest_planet.ID))
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
 
 
@@ -41,80 +38,37 @@ def spread_to_weakest_neutral_planet(state):
         return False
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
-        logging.info(strongest_planet.num_ships)
-        logging.info(weakest_planet.num_ships)
-        logging.info(state.distance(strongest_planet.ID, weakest_planet.ID))
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
 
 
 def attack_close_enemy_planet(state):
-    #attacked_planets = []
-    #for fleet in state.my_fleets():
-    #    attacked_planets.append(fleet.destination_planet)
-#    strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
-#    if strongest_planet == None:
-#        return False
     target_planets = [planet for planet in state.enemy_planets()
                       if not any(fleet.destination_planet == planet.ID for fleet in state.my_fleets())]
-    target_planets = iter(sorted(target_planets, key=lambda p: p.num_ships, reverse=True))
-
-    my_planets = iter(sorted(state.my_planets(), key=lambda p: p.num_ships))
+    target_planets = sorted(target_planets, key=lambda p: p.num_ships, reverse=True)
+    my_planets = sorted(state.my_planets(), key=lambda p: p.num_ships)
     if not my_planets:
         return False
-    target_planets = state.enemy_planets()
-    for planet in my_planets:
+    for source in my_planets:
         for target in target_planets:
-#            if target.ID in attacked_planets:
-#                continue
-            distance = state.distance(planet.ID, target.ID)
-            if distance <= 5 and planet.num_ships / 2 > target.num_ships:
-                #logging.info('found a candidate')
-                #logging.info(strongest_planet.num_ships)
-                #logging.info(target.num_ships)
-                return issue_order(state, planet.ID, target.ID, planet.num_ships / 2)
+            distance = state.distance(source.ID, target.ID)
+            required_ships = target.num_ships + distance * target.growth_rate + 1
+            if distance <= 5 and source.num_ships / 1.25 > required_ships:
+                return issue_order(state, source.ID, target.ID, required_ships)
     return False
-#  for target in target_planets:
-#      if target.ID in attacked_planets:
-#          continue
-#      distance = state.distance(strongest_planet.ID, target.ID)
-#      if distance <= 5 and strongest_planet.num_ships / 2 > target.num_ships:
-#           logging.info('found a candidate')
-#            logging.info(strongest_planet.num_ships)
-#            logging.info(target.num_ships)
-#            return issue_order(state, strongest_planet.ID, target.ID, strongest_planet.num_ships / 2)
 
 
 def attack_close_neutral_planet(state):
-    attacked_planets = []
-    for fleet in state.my_fleets():
-        attacked_planets.append(fleet.destination_planet)
-    #strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
-    #if strongest_planet is None:
-    #    return False
-    my_planets = iter(sorted(state.my_planets(), key=lambda p: p.num_ships))
+    target_planets = [planet for planet in state.neutral_planets()
+                      if not any(fleet.destination_planet == planet.ID for fleet in state.my_fleets())]
+    my_planets = sorted(state.my_planets(), key=lambda p: p.num_ships)
     if not my_planets:
         return False
-    target_planets = state.neutral_planets()
     for source in my_planets:
         for target in target_planets:
-            if target.ID in attacked_planets:
-                continue
             distance = state.distance(source.ID, target.ID)
-            if distance <= 5 and source.num_ships / 2 > target.num_ships:
-                #logging.info('found a candidate')
-                #logging.info(source.num_ships)
-                #logging.info(target.num_ships)
+            if distance <= 5 and source.num_ships / 1.25 > target.num_ships:
                 return issue_order(state, source.ID, target.ID, target.num_ships + 1)
     return False
-#for target in target_planets:
-#    if target.ID in attacked_planets:
-#        continue
-#    distance = state.distance(strongest_planet.ID, target.ID)
-#    if distance <= 5 and strongest_planet.num_ships / 2 > target.num_ships:
-#        logging.info('found a candidate')
-#        logging.info(strongest_planet.num_ships)
-#        logging.info(target.num_ships)
-#        return issue_order(state, strongest_planet.ID, target.ID, target.num_ships + 1)
 
 
 def wide_spread(state):
