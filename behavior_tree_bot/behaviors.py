229145df90703b92f2f2da859e7c9a04fe62e921
Harland Duman <dumanjacob@gmail.com>
2017-10-29 17:51:15 -0700

    Implemented basic close_planet strategy

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -1,4 +1,4 @@
-import sys
+import sys, logging
 sys.path.insert(0, '../')
 from planet_wars import issue_order
 
@@ -19,6 +19,9 @@ def attack_weakest_enemy_planet(state):
         return False
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
+        logging.info(strongest_planet.num_ships)
+        logging.info(weakest_planet.num_ships)
+        logging.info(state.distance(strongest_planet.ID, weakest_planet.ID))
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
 
 
@@ -38,4 +41,49 @@ def spread_to_weakest_neutral_planet(state):
         return False
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
+        logging.info(strongest_planet.num_ships)
+        logging.info(weakest_planet.num_ships)
+        logging.info(state.distance(strongest_planet.ID, weakest_planet.ID))
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+
+
+def attack_close_enemy_planet(state):
+    attacked_planets = []
+    for fleet in state.my_fleets():
+        attacked_planets.append(fleet.destination_planet)
+    strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
+    if strongest_planet == None:
+        return False
+    target_planets = state.enemy_planets()
+    for target in target_planets:
+        if target.ID in attacked_planets:
+            continue
+        distance = state.distance(strongest_planet.ID, target.ID)
+        if distance <= 5 and strongest_planet.num_ships / 2 > target.num_ships:
+            logging.info('found a candidate')
+            logging.info(strongest_planet.num_ships)
+            logging.info(target.num_ships)
+            return issue_order(state, strongest_planet.ID, target.ID, strongest_planet.num_ships / 2)
+    return False
+
+
+def attack_close_neutral_planet(state):
+    attacked_planets = []
+    for fleet in state.my_fleets():
+        attacked_planets.append(fleet.destination_planet)
+    strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
+    if strongest_planet == None:
+        return False
+    target_planets = state.neutral_planets()
+    logging.info(attacked_planets)
+    logging.info(target_planets)
+    for target in target_planets:
+        if target.ID in attacked_planets:
+            continue
+        distance = state.distance(strongest_planet.ID, target.ID)
+        if distance <= 5 and strongest_planet.num_ships / 2 > target.num_ships:
+            logging.info('found a candidate')
+            logging.info(strongest_planet.num_ships)
+            logging.info(target.num_ships)
+            return issue_order(state, strongest_planet.ID, target.ID, target.num_ships + 1)
+    return False
--- behavior_tree_bot/bt_bot.py
+++ behavior_tree_bot/bt_bot.py
@@ -25,6 +25,11 @@ def setup_behavior_tree():
     # Top-down construction of behavior tree
     root = Selector(name='High Level Ordering of Strategies')
 
+    close_planet_plan = Selector(name='Close Planet Strategy')
+    close_enemy_planet_attack = Action(attack_close_enemy_planet)
+    close_neutral_planet_attack = Action(attack_close_neutral_planet)
+    close_planet_plan.child_nodes = [close_enemy_planet_attack, close_neutral_planet_attack]
+
     offensive_plan = Sequence(name='Offensive Strategy')
     largest_fleet_check = Check(have_largest_fleet)
     attack = Action(attack_weakest_enemy_planet)
@@ -35,7 +40,7 @@ def setup_behavior_tree():
     spread_action = Action(spread_to_weakest_neutral_planet)
     spread_sequence.child_nodes = [neutral_planet_check, spread_action]
 
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    root.child_nodes = [close_planet_plan, offensive_plan, spread_sequence, attack.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
